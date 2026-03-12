1377a256cc59f37be4e87e106b2240ab760b5fd5
unknown <wadepaw@gmail.com>
2026-03-12 14:20:50 +0100

    login

---
+++ frontend/src/app/login/page.tsx
@@ -0,0 +1,10 @@
+import LoginForm from "./login/LoginForm";
+
+export default function Page() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold text-center mt-8">Connexion PawLegal</h1>
+      <LoginForm />
+    </div>
+  );
+}
